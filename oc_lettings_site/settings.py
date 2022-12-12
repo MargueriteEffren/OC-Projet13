755bd71bd564b2a027d48156aa261ba70035d6be
Marguerite Effren <pascaline_vigouroux@hotmail.com>
2022-12-12 16:44:38 +0100

    découpage de templates, renommage de profiles

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -26,9 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'oc_profile',
-    'lettings',
     'profiles',
+    'lettings',
 ]
 
 MIDDLEWARE = [
